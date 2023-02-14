872de66da74ee83b1354fe65eb981664a44caade
chanma <yunosuke.homma@icloud.com>
2023-02-14 18:46:19 +0900

    add test

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -4,6 +4,7 @@
 
 #include "Fixed.hpp"
 #include <iostream>
+
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
@@ -14,5 +15,25 @@ int main( void ) {
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "--------additional-------" << std::endl;
+	Fixed const c( Fixed( 5.05f ) / Fixed( 2 ) );
+	std::cout << c << std::endl;
+	// a(0) > b(10.1016) -> false
+	if (a > b)
+		std::cout << "a > b: " << "true" << std::endl;
+	else
+		std::cout << "a > b: " << "false" << std::endl;
+	// a(0) < b(10.1016) -> true
+	if (a < b)
+		std::cout << "a < b: " << "true" << std::endl;
+	else
+		std::cout << "a < b: " << "false" << std::endl;
+	// 	// a(0) == b(10.1016) false
+	if (a== b)
+		std::cout << "a < b: " << "true" << std::endl;
+	else
+		std::cout << "a < b: " << "false" << std::endl;
 	return 0;
+
+
 }
