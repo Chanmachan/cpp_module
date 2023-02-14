8f41d5bc8b3e5560eb3569450adc69743258cb00
chanma <yunosuke.homma@icloud.com>
2023-02-14 13:54:56 +0900

    add

---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 本間優之介 on 2022/12/21.
+//
+
+#include "Fixed.hpp"
+#include <iostream>
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
