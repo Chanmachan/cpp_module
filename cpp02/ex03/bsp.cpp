76f78918b2c30234ba6c587ef10e08b127e5d8de
chanma <yunosuke.homma@icloud.com>
2023-02-20 00:14:04 +0900

    change func name

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -5,7 +5,7 @@
 #include "Point.h"
 
 // ベクトルの外積の計算
-Fixed judge_outer_product_sign(Point p1, Point p2, Point p3) {
+Fixed outer_product(Point p1, Point p2, Point p3) {
 	return (p1.get_x() - p3.get_x()) * (p2.get_y() - p3.get_y()) \
 			- (p2.get_x() - p3.get_x()) * (p1.get_y() - p3.get_y());
 }
@@ -14,9 +14,9 @@ Fixed judge_outer_product_sign(Point p1, Point p2, Point p3) {
 bool bsp( Point const& a, Point const& b, Point const& c, Point const& point) {
 	Fixed p1, p2, p3;
 
-	p1 = judge_outer_product_sign(point, a, b);
-	p2 = judge_outer_product_sign(point, b, c);
-	p3 = judge_outer_product_sign(point, c, a);
+	p1 = outer_product(point, a, b);
+	p2 = outer_product(point, b, c);
+	p3 = outer_product(point, c, a);
 	if ((p1 > 0 && p2 > 0 && p3 > 0) || (p1 < 0 && p2 < 0 && p3 < 0))
 		return true;
 	return false;
