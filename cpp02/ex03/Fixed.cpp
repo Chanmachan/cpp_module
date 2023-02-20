c5ec4008a87bc33c7e23072f9e603676e0530279
chanma <yunosuke.homma@icloud.com>
2023-02-20 15:44:37 +0900

    /operator内ではthrowのみの処理に->ex03にも適用

--- cpp02/ex03/Fixed.cpp
+++ cpp02/ex03/Fixed.cpp
@@ -157,13 +157,10 @@ Fixed Fixed::operator*(const Fixed& a) const {
 
 Fixed Fixed::operator/(const Fixed& a) const {
 	Fixed ret;
-	try {
-		if (a.getRawBits() == 0)
-			throw std::invalid_argument("Floating point exception");
-		ret.setRawBits((this->fixed_num << fractional_bits) / a.getRawBits());
-	} catch (std::invalid_argument&) {
-		std::cout << "FPE: Dividing by 0" << std::endl;
+	if (a.getRawBits() == 0) {
+		throw std::invalid_argument("Floating point exception");
 	}
+	ret.setRawBits((this->fixed_num << fractional_bits) / a.getRawBits());
 	return ret;
 }
 
