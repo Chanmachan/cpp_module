2a84caa9f03015d2b88ea20efa620ff545dc4f7f
chanma <yunosuke.homma@icloud.com>
2023-02-25 11:19:41 +0900

    create files

---
+++ cpp05/ex02/AForm.h
@@ -0,0 +1,55 @@
+//
+// Created by 本間優之介 on 2023/02/23.
+//
+
+#ifndef CPP05EX01_FORM_H
+#define CPP05EX01_FORM_H
+
+#include <iostream>
+
+class Bureaucrat;
+
+class AForm {
+private:
+	const std::string name_;
+	bool signed_flag_;
+	const int grade_to_sign_;
+	const int grade_to_exec_;
+	AForm();
+public:
+	AForm(const AForm& src);
+	AForm(const std::string& name, int grade_to_sign, int grade_to_exec);
+	~AForm();
+
+	AForm& operator=(const AForm& src);
+
+	const std::string& getName() const;
+	int getSignedFlag() const;
+	int getGradeToSign() const;
+	int getGradeToExec() const;
+
+	void beSigned(const Bureaucrat& Bureaucrat);
+
+	class GradeTooHighException: public std::out_of_range {
+	private:
+		GradeTooHighException& operator=(const GradeTooHighException& src);
+	public:
+		GradeTooHighException();
+		GradeTooHighException(const AForm::GradeTooHighException& src);
+		~GradeTooHighException() _NOEXCEPT;
+	};
+
+	class GradeTooLowException: public std::out_of_range {
+	private:
+		GradeTooLowException& operator=(const GradeTooLowException& src);
+	public:
+		GradeTooLowException();
+		GradeTooLowException(const AForm::GradeTooLowException& src);
+		~GradeTooLowException() _NOEXCEPT;
+	};
+};
+
+std::ostream& operator<<(std::ostream& ostream, const AForm& Form);
+
+
+#endif //CPP05EX01_FORM_H
