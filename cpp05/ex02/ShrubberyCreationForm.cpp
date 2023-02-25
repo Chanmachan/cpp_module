a48b6ea97d97e23c26c3a0c1df8a1b6f0e95ba1d
chanma <yunosuke.homma@icloud.com>
2023-02-25 11:54:58 +0900

    add shrubberry proto

--- cpp05/ex02/ShrubberyCreationForm.cpp
+++ cpp05/ex02/ShrubberyCreationForm.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "ShrubberyCreationForm.h"
+
+ShrubberyCreationForm::ShrubberyCreationForm()
+	: AForm("no_name_shrubbery",
+			required_grade_to_sign,
+			required_grade_to_exec) {
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target)
+	: AForm(target + "_shruberry",
+			required_grade_to_sign,
+			required_grade_to_exec) {
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& src): AForm(src) {
+
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {
+
+}
--- cpp05/ex02/ShrubberyCreationForm.h
+++ cpp05/ex02/ShrubberyCreationForm.h
@@ -5,12 +5,19 @@
 #ifndef CPP05EX01_SHRUBBERYCREATIONFORM_H
 #define CPP05EX01_SHRUBBERYCREATIONFORM_H
 
+#include "AForm.h"
 
-class ShrubberyCreationForm {
+class ShrubberyCreationForm: public AForm {
 private:
-
+	const static int required_grade_to_sign = 145;
+	const static int required_grade_to_exec = 137;
 public:
 	ShrubberyCreationForm();
+	ShrubberyCreationForm(const std::string& target);
+	ShrubberyCreationForm(const ShrubberyCreationForm& src);
+	~ShrubberyCreationForm();
+
+	ShrubberyCreationForm& operator=(const ShrubberyCreationForm& src);
 };
 
 
