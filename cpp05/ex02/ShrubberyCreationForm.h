2ddfa5bb7c36e2c16eab834ad17b8a1dd75f8fa4
chanma <yunosuke.homma@icloud.com>
2023-02-27 03:03:14 +0900

    comp

--- cpp05/ex02/ShrubberyCreationForm.h
+++ cpp05/ex02/ShrubberyCreationForm.h
@@ -6,18 +6,23 @@
 #define CPP05EX01_SHRUBBERYCREATIONFORM_H
 
 #include "AForm.h"
+// ファイルの読み書きが必要
+#include <fstream>
 
 class ShrubberyCreationForm: public AForm {
 private:
+	const std::string target_;
 	const static int required_grade_to_sign = 145;
 	const static int required_grade_to_exec = 137;
 public:
 	ShrubberyCreationForm();
-	ShrubberyCreationForm(const std::string& target);
-	ShrubberyCreationForm(const ShrubberyCreationForm& src);
+	ShrubberyCreationForm(const std::string &target);
+	ShrubberyCreationForm(const ShrubberyCreationForm &src);
 	~ShrubberyCreationForm();
 
-	ShrubberyCreationForm& operator=(const ShrubberyCreationForm& src);
+	ShrubberyCreationForm &operator=(const ShrubberyCreationForm &src);
+
+	void execute(Bureaucrat const & executor) const;
 };
 
 
