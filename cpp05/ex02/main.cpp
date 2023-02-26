4a5186103d9472816872dfedaff93ce5fd873f03
chanma <yunosuke.homma@icloud.com>
2023-02-27 03:02:36 +0900

    modify main

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -3,9 +3,60 @@
 //
 
 #include "Bureaucrat.h"
+#include "AForm.h"
 #include "ShrubberyCreationForm.h"
+#include "RobotomyRequestForm.h"
+#include "PresidentialPardonForm.h"
 
 int main() {
-	ShrubberyCreationForm a;
-	ShrubberyCreationForm b(a);
+	{
+		ShrubberyCreationForm a("test1");
+		ShrubberyCreationForm b(a);
+		ShrubberyCreationForm c;
+		Bureaucrat d("Kishida", 10);
+
+		c = a;
+
+		d.signForm(c);
+		try {
+			c.execute(d);
+		} catch (std::exception& e) {
+			std::cout << "exception: " << e.what() << std::endl;
+			std::cout << d.getName() << " (" << d.getGrade() << ")" << " has not reached " << c.getGradeToExec() << " grade_to_exec" << std::endl;
+		}
+	}
+	std::cout << std::endl << "------------------------------------------------------" << std::endl << std::endl;
+//	{
+//		RobotomyRequestForm a("hoge");
+//		RobotomyRequestForm b(a);
+//		RobotomyRequestForm c;
+//		Bureaucrat d("Kishida", 10);
+//
+//		c = a;
+
+//		d.signForm(c);
+//		try {
+//			c.execute(d);
+//		} catch (std::exception& e) {
+//			std::cout << "hogexception\n";
+//			std::cout << e.what() << std::endl;
+//		}
+//	}
+//	std::cout << std::endl << "------------------------------------------------------" << std::endl << std::endl;
+	{
+		PresidentialPardonForm a("test3");
+		PresidentialPardonForm b(a);
+		PresidentialPardonForm c;
+		Bureaucrat d("Kishida", 4);
+
+		c = a;
+
+		d.signForm(c);
+		try {
+			c.execute(d);
+		} catch (std::exception& e) {
+			std::cout << "exception: " << e.what() << std::endl;
+			std::cout << d.getName() << " (" << d.getGrade() << ")" << " has not reached " << c.getGradeToExec() << " grade_to_exec" << std::endl;
+		}
+	}
 }
