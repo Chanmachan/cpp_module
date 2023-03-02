bee87a681bfc00767c4bbd2edb3058ff843d17ca
chanma <yunosuke.homma@icloud.com>
2023-03-02 14:45:55 +0900

    modify

--- cpp04/ex02/Cat.cpp
+++ cpp04/ex02/Cat.cpp
@@ -5,29 +5,26 @@
 #include "Cat.h"
 
 Cat::Cat(): Animal("Cat"), brains_(new (std::nothrow) Brain()) {
-	std::cout << "Hi Cat !" << std::endl;
+	std::cout << "Cat " << type << " Constructor called" << std::endl;
 }
 
 Cat::Cat(const Cat &src)  : Animal(src) {
-	std::cout << "Cat " << this->type << " is duplicated"<< std::endl;
-	this->Animal::type = src.Animal::type;
-	// srcのbrains_を複製 -> ()必要？*(src.brains_)
-	if (src.brains_) {
-		brains_ = new(std::nothrow) Brain(*src.brains_);
-	}
+	std::cout << "Cat " << type << " Copy Constructor called"<< std::endl;
+	this->type = src.type;
+	brains_ = new(std::nothrow) Brain(*src.brains_);
 }
 
 Cat::~Cat() {
-	std::cout << "Bye Cat !" << std::endl;
+	std::cout << "Cat " << type << " Destructor called" << std::endl;
 	// deleteはfreeと違ってNULLでも問題ない
 	delete brains_;
 }
 
 Cat& Cat::operator=(const Cat &src) {
 	std::cout << "Cat " << this->type << " operator= called" << std::endl;
-	this->Animal::type = src.Animal::type;
-	if (src.brains_) {
-		this->brains_ = new(std::nothrow) Brain(*src.brains_);
+	if (this != &src) {
+		this->type = src.type;
+		*this->brains_ = *src.brains_;
 	}
 	return *this;
 }
