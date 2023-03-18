28f8ea81e7189f1ca67e057c2987d012c41a8fb6
chanma <yunosuke.homma@icloud.com>
2023-03-18 23:33:17 +0900

    一応参照が引数できたとき用のiterを追加

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -21,4 +21,18 @@ void iter(const T* array, size_t len, void (*f)(const T&)) {
 	}
 }
 
+template<typename T>
+void iter(T* array, size_t len, void (*f)(T)) {
+	for (size_t i = 0; i < len; i++) {
+		f(array[i]);
+	}
+}
+
+template<typename T>
+void iter(const T* array, size_t len, void (*f)(const T)) {
+	for (size_t i = 0; i < len; i++) {
+		f(array[i]);
+	}
+}
+
 #endif //EXE_ITER_HPP
