eb50d2d99de625fe6efb1bf287a6f42ec9578a6e
chanma <yunosuke.homma@icloud.com>
2024-04-10 17:23:10 +0900

    元のデータをいじってないのでソートされてないままテストされるけど一旦出力確認用でコミット、動作は確認できた

--- cpp09/ex02/test/test.cpp
+++ cpp09/ex02/test/test.cpp
@@ -31,4 +31,32 @@ BOOST_AUTO_TEST_SUITE(PmergeMeSuite)
 		BOOST_CHECK_THROW(pmergeMeInstance.inputVec(3, (char **)args), std::invalid_argument);
 	}
 
+	BOOST_AUTO_TEST_CASE(TestMergeInsertionSort) {
+
+		std::vector<int> testData;
+		testData.push_back(5);
+		testData.push_back(3);
+		testData.push_back(2);
+		testData.push_back(1);
+		testData.push_back(7);
+		testData.push_back(6);
+		testData.push_back(9);
+		testData.push_back(8);
+		std::vector<int> expectedResult;
+		expectedResult.push_back(5);
+		expectedResult.push_back(3);
+		expectedResult.push_back(2);
+		expectedResult.push_back(1);
+		expectedResult.push_back(7);
+		expectedResult.push_back(6);
+		expectedResult.push_back(9);
+		expectedResult.push_back(8);
+
+		// mergeInsertionSort関数を実行
+		PmergeMe::mergeInsertionSort(testData);
+
+		// ソートされた結果を確認
+		BOOST_CHECK_EQUAL_COLLECTIONS(testData.begin(), testData.end(), expectedResult.begin(), expectedResult.end());
+	}
+
 BOOST_AUTO_TEST_SUITE_END()
