95598de170705a76fb7e916f366f2857c773024c
chanma <yunosuke.homma@icloud.com>
2024-04-11 09:46:06 +0900

    explicitとメモを追加

--- cpp09/ex02/PairComparisonResult.hpp
+++ cpp09/ex02/PairComparisonResult.hpp
@@ -14,7 +14,8 @@ private:
 
 public:
 	PairComparisonResult(Iterator winner, Iterator loser): itrWinner_(winner), itrLoser_(loser), size_(2) {};
-	PairComparisonResult(Iterator winner): itrWinner_(winner), size_(1) {};
+	// 引数が一つのときの暗黙的な型変換防止でexplicit
+	explicit PairComparisonResult(Iterator winner): itrWinner_(winner), size_(1) {};
 
 	int getSize() { return size_; };
 	// イテレーターの先
