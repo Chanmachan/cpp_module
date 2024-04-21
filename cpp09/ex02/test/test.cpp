00a9ca56278bef13925de874d6ac35ea782af8ad
chanma <yunosuke.homma@icloud.com>
2024-04-21 22:57:47 +0900

    テストコードをコードの変更に合わせて大幅変更

--- cpp09/ex02/test/test.cpp
+++ cpp09/ex02/test/test.cpp
@@ -7,148 +7,67 @@
 #include "../PmergeMe.hpp"
 #include <vector>
 
-BOOST_AUTO_TEST_SUITE(PmergeMeSuite)
+
+BOOST_AUTO_TEST_SUITE(TestSuitePmergeMe)
 
 	BOOST_AUTO_TEST_CASE(TestValidInput) {
-		PmergeMe pmergeMeInstance;
+		PmergeMe pm;
+		const char* args[] = {"3", "1", "4", "1", "5", NULL};
+		std::vector<int> vec;
+		// argsをstd::vectorに変換する関数を呼び出し
+		pm.inputToContainer<int, std::vector>(5, const_cast<char**>(args), vec);
 
-		const char *args[] = {"3", "1", "4", "1", "5"};
-		std::vector<int> expectedResult;
-		expectedResult.push_back(3);
-		expectedResult.push_back(1);
-		expectedResult.push_back(4);
-		expectedResult.push_back(1);
-		expectedResult.push_back(5);
-		std::vector<int> result = pmergeMeInstance.inputVec(5, (char **)args);
+		std::vector<int> expected;
+		expected.push_back(3);
+		expected.push_back(1);
+		expected.push_back(4);
+		expected.push_back(1);
+		expected.push_back(5);
 
-		BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expectedResult.begin(), expectedResult.end());
+		BOOST_CHECK_EQUAL_COLLECTIONS(vec.begin(), vec.end(), expected.begin(), expected.end());
 	}
 
 	BOOST_AUTO_TEST_CASE(TestInvalidInput) {
-		PmergeMe pmergeMeInstance;
-
-		const char *args[] = {"1", "a", "2"};
-		BOOST_CHECK_THROW(pmergeMeInstance.inputVec(3, (char **)args), std::invalid_argument);
+		PmergeMe pm;
+		const char* args[] = {"1", "a", "2", NULL};
+		std::vector<int> vec;
+		BOOST_CHECK_THROW(pm.inputToContainer(3, const_cast<char **>(args), vec), std::invalid_argument);
 	}
 
-	BOOST_AUTO_TEST_CASE(TestMergeInsertionSort) {
-
-		std::vector<int> testData;
-		testData.push_back(5);
-		testData.push_back(3);
-		testData.push_back(2);
-		testData.push_back(1);
-		testData.push_back(7);
-		testData.push_back(6);
-		testData.push_back(9);
-		testData.push_back(8);
-		testData.push_back(10);
-		std::vector<int> expectedResult;
-		expectedResult.push_back(5);
-		expectedResult.push_back(3);
-		expectedResult.push_back(2);
-		expectedResult.push_back(1);
-		expectedResult.push_back(7);
-		expectedResult.push_back(6);
-		expectedResult.push_back(9);
-		expectedResult.push_back(8);
-		expectedResult.push_back(10);
-
-		// mergeInsertionSort関数を実行
-		std::vector<int> ret = PmergeMe::mergeInsertionSort(testData);
-
-		// ソートされた結果を確認
-		BOOST_CHECK_EQUAL_COLLECTIONS(testData.begin(), testData.end(), expectedResult.begin(), expectedResult.end());
-	}
-
-	BOOST_AUTO_TEST_CASE(TestInsertionSort)
-	{
+	BOOST_AUTO_TEST_CASE(TestMergeInsertionSortVector) {
+		PmergeMe pm;
 		std::vector<int> data;
 		data.push_back(5);
 		data.push_back(3);
-		data.push_back(2);
-		data.push_back(1);
-		data.push_back(7);
-		data.push_back(6);
 		data.push_back(9);
-		data.push_back(8);
-		data.push_back(10);
-		std::vector<int> expectedResult;
-		expectedResult.push_back(1);
-		expectedResult.push_back(2);
-		expectedResult.push_back(3);
-		expectedResult.push_back(5);
-		expectedResult.push_back(6);
-		expectedResult.push_back(7);
-		expectedResult.push_back(8);
-		expectedResult.push_back(9);
-		expectedResult.push_back(10);
-
-		// 実際のソート関数を呼び出す
-		std::vector<int> result;
-		result = PmergeMe::mergeInsertionSort(data);
-
-		// 結果を検証する
-		BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expectedResult.begin(), expectedResult.end());
-	}
-
-	BOOST_AUTO_TEST_CASE(TestSimpleInput) {
-		std::vector<int> input;
-		input.push_back(5);
-		input.push_back(2);
-		input.push_back(3);
-		input.push_back(1);
-		input.push_back(4);
+		data.push_back(1);
+		pm.mergeInsertionSort<int, std::vector>(data);
 
 		std::vector<int> expected;
 		expected.push_back(1);
+		expected.push_back(3);
+		expected.push_back(5);
+		expected.push_back(9);
+
+		BOOST_CHECK_EQUAL_COLLECTIONS(data.begin(), data.end(), expected.begin(), expected.end());
+	}
+
+	BOOST_AUTO_TEST_CASE(TestMergeInsertionSortList) {
+		PmergeMe pm;
+		std::list<int> data;
+		data.push_back(10);
+		data.push_back(2);
+		data.push_back(5);
+		data.push_back(3);
+		pm.mergeInsertionSort<int, std::list>(data);
+
+		std::list<int> expected;
 		expected.push_back(2);
 		expected.push_back(3);
-		expected.push_back(4);
 		expected.push_back(5);
+		expected.push_back(10);
 
-		std::vector<int> result = PmergeMe::mergeInsertionSort(input);
-		BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expected.begin(), expected.end());
+		BOOST_CHECK_EQUAL_COLLECTIONS(data.begin(), data.end(), expected.begin(), expected.end());
 	}
-
-	BOOST_AUTO_TEST_CASE(TestEmptyInput) {
-		std::vector<int> input;
-
-		std::vector<int> expected;
-
-		std::vector<int> result = PmergeMe::mergeInsertionSort(input);
-		BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expected.begin(), expected.end());
-	}
-
-	BOOST_AUTO_TEST_CASE(TestAlreadySortedInput) {
-		std::vector<int> input;
-		for(int i = 1; i <= 5; ++i) {
-			input.push_back(i);
-		}
-
-		std::vector<int> expected;
-		for(int i = 1; i <= 5; ++i) {
-			expected.push_back(i);
-		}
-
-		std::vector<int> result = PmergeMe::mergeInsertionSort(input);
-		BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expected.begin(), expected.end());
-	}
-
-	BOOST_AUTO_TEST_CASE(TestReverseSortedInput) {
-		std::vector<int> input;
-		for(int i = 5; i >= 1; --i) {
-			input.push_back(i);
-		}
-
-		std::vector<int> expected;
-		for(int i = 1; i <= 5; ++i) {
-			expected.push_back(i);
-		}
-
-		std::vector<int> result = PmergeMe::mergeInsertionSort(input);
-		BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expected.begin(), expected.end());
-	}
-
 
 BOOST_AUTO_TEST_SUITE_END()
